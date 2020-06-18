c01eca2f69c0326a7c649c4ba612d77c929e8bcc
HR1Aventum <rohan.arya237@gmail.com>
2020-06-18 23:15:53 +0530

    ~

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.json());
+let productData = [];
+
+// app.get('/users',(req,res)=>{
+//     res.json([
+//         {
+//             name:"Person 1",
+//             age:20
+//         },
+//         {
+//             name:"Person 2",
+//             age:30
+//         },
+//         {
+//             name:"Person 3",
+//             age:12
+//         }
+//     ])
+// });
+
+// app.get('/product',(req,res)=>{
+//     productData.push(req.body);
+// } )
+
+
+app.post('/create',(req,res) => {
+    fs.writeFile(`${req.body.filename}.${req.body.type}`,req.body.content,(err)=>{
+        if(err) throw err;
+        res.json({
+            message:"file created!"
+        })
+    } )
+})
+
+app.get('/product',(req,res)=>{
+     res.json(productData)
+})
+
+app.listen(3000,()=>{
+    console.log("check @ port 3000")
+})
